76c927a9cd05263f1adf94ae55096357ff157f0b
Vadym Beletskyi <mrdexters1@gmail.com>
2025-08-11 18:41:35 +0200

    feat: itfam init

---
+++ apps/itfam/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+		<h1 className="font-mono text-center">Wordpress & Shopify Solutions</h1>
+		<h2 className="font-mono text-center">Design, Development and Setup</h2>
+      </main>
+    </div>
+  );
+}
--- apps/top-tokens/app/page.tsx
+++ apps/top-tokens/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        hardware - picker - ai
         <Hero />
       </main>
     </div>
